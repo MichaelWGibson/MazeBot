a255d63b162a28eefd2a9e48afaed5f492f8054a
Michael Gibson <mgibson@selectbankcard.com>
2020-10-26 09:53:55 -0600

    Added a drive train

---
+++ src/Drive/drive_train.py
@@ -0,0 +1,42 @@
+import math
+
+class DriveTrain():
+    """ Represents a drive train, responsible for controlling motors """
+    def __init__(self, left_motor, right_motor):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+
+    def set_heading(self, speed, angle):
+        """
+        Drive in a certain direction at a certain speed
+
+        Args:
+            speed (int): The speed to drive at from 0 - 1 inclusive
+            angle (int): Direction to go 0 being straight, -π/2 => left, π/2 => right
+        """
+
+        # Check inputs
+        assert 0 <= speed <= 1, "Argument 'speed' is out of range"
+        assert -math.pi <= angle <= math.pi, "Argument 'angle' is out of range"
+
+        # Use trig to break down the triangle
+        forward_component = math.cos(angle)
+        side_component = math.sin(angle)
+
+        # Put it back together in terms of motor speed
+        # If the side component is right, spin left wheel faster
+        left_speed = forward_component + side_component
+        right_speed = forward_component - side_component
+
+        # Check for overflows
+        left_speed = min(1, left_speed)
+        right_speed = min(1, right_speed)
+
+        left_speed = max(-1, left_speed)
+        right_speed = max(-1, right_speed)
+
+        # Print results
+        print("Motor speeds, Left: {:.2f} Right: {:.2f}".format(left_speed, right_speed))
+
+        # Todo: Actually set the motor values
+        
