cfd22096af2bd61d08440146b623615142750a33
Michael Gibson <mgibson@selectbankcard.com>
2020-10-26 12:01:41 -0600

    Added mock motor class

---
+++ src/Drive/mock_motor.py
@@ -0,0 +1,15 @@
+
+class MockMotor():
+    """
+    A class that represents a mock motor.
+    """
+
+    def __init__(self):
+        """
+        Does nothing
+        """
+
+    def set_speed(self, speed):
+        """
+        Does nothing
+        """
