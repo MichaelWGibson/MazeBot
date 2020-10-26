76257a2be4fd1077a57fd8ab2aed46c9d3e3f0af
Michael Gibson <mgibson@selectbankcard.com>
2020-10-26 11:36:23 -0600

    Pass joystick input to drive code

--- src/Stream/stream.py
+++ src/Stream/stream.py
@@ -1,33 +1,47 @@
+import math
 from flask import Flask, render_template, Response, request
-import cv2
-import time
-import threading
-from threading import Thread
 
 # The flask app
 app = Flask(__name__)
 
-# Video feed generator
-source = None
+# Globals
+SOURCE = None
+DRIVE = None
+
 
 @app.route('/')
 def index():
     """ Default route """
     return render_template('index.html')
 
+
 @app.route("/video_feed")
 def video_feed():
     """ Serves the video feed """
-    return Response(source(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(SOURCE(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 @app.route("/heading", methods=['POST'])
 def set_heading():
+    """ Receives a raw heading from the joystick """
+    # Get data from form
     speed = request.form.get('speed',  type=float)
     angle = request.form.get('angle',  type=float)
-    print("Setting heading, Speed: {:.2f} Angle: {:.2f}".format(speed, angle))
-    return("done")
 
-def start(image_source):
+    # Cap speed at a force of 3, scale range [0, 1]
+    speed = min(speed, 3) / 3
+
+    # Rotate one quarter turn
+    angle = (angle + math.pi/2) % (math.pi * 2)
+
+    # Invert and center for a range of [-π, π]
+    angle = -angle + math.pi
+
+    DRIVE.set_heading(speed, angle)
+    return "done"
+
+
+def start(image_source, drive_train):
     """
     Start the flask server and begin serving the video feed
 
@@ -35,8 +49,12 @@ def start(image_source):
         image_source (generator): A generator that produces the video stream to serve
     """
     # I know globals are bad, but making flask into a class looked hard
-    global source
-    source = image_source
+    global SOURCE
+    SOURCE = image_source
 
-    # Start flask server publicly on port 5000, don't reload so we don't 
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded = True, use_reloader=False)
+    global DRIVE
+    DRIVE = drive_train
+
+    # Start flask server publicly on port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True,
+            threaded=True, use_reloader=False)
--- src/maze_bot.py
+++ src/maze_bot.py
@@ -1,11 +1,13 @@
 import config
 from Vision.video_feed import VideoFeed
+from Drive.drive_train import DriveTrain
 import Stream.stream as stream
 
 try:
+    drive_train = DriveTrain(None, None)
     cam = config.get_camera()
     feed = VideoFeed(cam)
-    stream.start(feed.get_generator)
+    stream.start(feed.get_generator, drive_train)
 except KeyboardInterrupt:
     print('Shutting down')
     feed.close()
